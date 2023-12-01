d09309e9af30f17251fc3ae606a0adb515276984
Ranafonica <127953603+Ranafonica@users.noreply.github.com>
2023-11-30 21:57:26 -0300

    Actualizacion Definitiva
    cuenta con Grafos, grafos en forma de matriz, enumeracion de Ciudades por Numeros y Listas de Guardianes

--- Proyecto 2.cpp
+++ Proyecto 2.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-// Estructuras:
+//---------------------------------------------ESTRUCTURAS---------------------------------------------
 // Estructura para almacenar la información de los Guardianes.
 struct Guardian {
     string name;
@@ -21,6 +21,7 @@ struct Guardian {
     string city;
     vector<string> apprentices;
 };
+
 // Estructura para almacenar la información de los Nodos.
 struct Node {
     Guardian guardian;
@@ -28,6 +29,7 @@ struct Node {
     Node* right;
     Node(const Guardian& guard) : guardian(guard), left(nullptr), right(nullptr) {}
 };
+
 // Estructura para almacenar la información del arbol de guardianes.
 struct GuardianNodoArbol {
     Guardian guardian;																	// Almacena la información del guardian.
@@ -35,6 +37,115 @@ struct GuardianNodoArbol {
     GuardianNodoArbol* master;															// Almacena el maestro del guardian.
     GuardianNodoArbol(const Guardian& guard) : guardian(guard), master(nullptr) {}
 };
+
+//---------------------------------------------GRAFOS---------------------------------------------
+using Grafo = unordered_map<string, unordered_set<string>>;
+
+//Funcion que carga los datos del archivo "Ciudades.txt".
+void lecturaCiudad(const string &nombreArchivo, Grafo &grafo, unordered_set<string> &uniqueCity) {
+    ifstream fileCiudad("Ciudades.txt");
+    if (!fileCiudad.is_open()) {
+    	cout << endl;
+    	cout << "Archivo Ciudades.txt no encontrado." << endl;
+        cout << "No se ha podido abrir correctamente." << endl;
+        return;
+    } else {
+    	cout << "Archivo Ciudades.txt encontrado.";
+        cout << "...Archivo abierto satisfactoriamente" << endl;
+        cout << endl;
+    }
+    
+    // Formato de lectura de ciudades.
+    string line;
+    while (getline(fileCiudad, line)) {
+        istringstream ss(line);
+        string cityA, cityB;
+        getline(ss, cityA, ',');
+        getline(ss, cityB);
+
+        
+        grafo[cityA].insert(cityB);
+        grafo[cityB].insert(cityA);
+
+        
+        uniqueCity.insert(cityA);
+        uniqueCity.insert(cityB);
+    }
+    fileCiudad.close();
+}
+
+// Funcion para la impresion de grafo general.
+void imprimirGrafo(const Grafo &grafo) {
+    cout << "[GRAFO PRINCIPAL]: \n";
+    cout << endl;
+    for (const auto &par : grafo) {
+        cout << par.first << ": ";
+        for (const auto &vecino : par.second) {
+            cout << vecino << " -> ";
+        }
+        cout << "\n";
+    }
+}
+
+// Funcion que crea la Matriz de comparación.
+void MatrizComparacion(const unordered_set<string> &uniqueCity, const Grafo &grafo, const unordered_map<string, int> &numCiudad) {
+	cout << "\n[MATRIZ CON NUMEROS]: \n";
+	cout << endl;
+	// Impresion de Numeros de forma Horizontal
+    cout << "   ";
+    for (const auto &ciudad : uniqueCity) {
+        cout << numCiudad.at(ciudad) << " ";
+    }
+    cout << "\n";
+    
+    // Imprimir de Numeros de forma Vertical
+    for (const auto &ciudad : uniqueCity) {
+        cout << numCiudad.at(ciudad) << ": ";
+        
+        for (const auto &otraCiudad : uniqueCity) {
+            cout << (grafo.at(ciudad).count(otraCiudad) > 0 ? "X " : "O ");
+            
+        }
+        cout << "\n";
+    }
+}
+
+// Función para cargar la lista de guardianes desde el archivo "Guardianes.txt"
+vector<pair<int, Guardian>> leerGuardianesDesdeArchivo(const string& nombreArchivo) {
+    ifstream fileGuardian(nombreArchivo);
+    vector<pair<int, Guardian>> guardianes;
+
+    if (!fileGuardian.is_open()) {
+        cout << "Archivo Guardianes.txt no encontrado." << endl;
+        cout << "No se ha podido abrir correctamente." << endl;
+    } else {
+        cout << "Archivo Guardianes.txt encontrado.";
+        cout << "...Archivo abierto satisfactoriamente" << endl;
+
+        string line;
+        getline(fileGuardian, line); // Saltar la primera línea que contiene encabezados
+
+        int numero = 1;
+        while (getline(fileGuardian, line)) {
+            istringstream ss(line);
+            Guardian guardian;
+            getline(ss, guardian.name, ',');
+            ss >> guardian.powerLevel;
+            ss.ignore(); // Ignorar la coma después del nivel de poder
+            getline(ss, guardian.master, ',');
+            getline(ss, guardian.city);
+
+            guardianes.push_back({numero, guardian});
+            numero++;
+        }
+
+        fileGuardian.close();
+    }
+
+    return guardianes;
+}
+
+// ---------------------------------------------FUNCIONES PRINCIPALES---------------------------------------------
 // Insertar un nuevo nodo en el arbol binario bajo el parametro del nivel de poder.
 void InsertNode(Node*& root, const Guardian& guardian){
 	if (root == nullptr){																// Se crea un nuevo nodo con la informacion del guardian y se establece como raiz.
@@ -47,13 +158,14 @@ void InsertNode(Node*& root, const Guardian& guardian){
         InsertNode(root->right, guardian);
     }
 }
+
 // Recorre de manera inversa el arbol, es decir, de derecha a izquierda.
 // Aqui se hace la distincion entre los Guardianes del Reino y los Candidatos a Reino.
 void DescendentPower(Node* root, int& count, int& countReino){
 	if (root == nullptr || count == 0){
 		return;
 	}
-	DescendentPower(root->right, count, countReino);								// Se imprime.
+	DescendentPower(root->right, count, countReino);									// Se imprime.
 	
 	if (root->guardian.powerLevel >= 90 && root->guardian.powerLevel <= 100) {			// Se establece la distincion entre puntos de poder para ser Guardian de Reino o Candidato.
         if (countReino < 3) {
@@ -90,30 +202,38 @@ bool MaestroGuardian (const Guardian& guardian, Node* guardianTree){
 	}
 	return MaestroGuardian (guardian, guardianTree->left) || MaestroGuardian (guardian, guardianTree->right);
 }
-int main (){
-	
-	ifstream fileCiudad("Ciudades.txt");
-    if (!fileCiudad.is_open()) {
-    	cout << "Archivo Ciudades.txt no encontrado." << endl;
-        cout << "No se ha podido abrir correctamente." << endl;
-        return 1;
-    } else {
-    	cout << "Archivo Ciudades.txt encontrado.";
-        cout << "...Archivo abierto satisfactoriamente" << endl;
-    }
-    // Funcion que carga los datos del archivo "Guardianes.txt"
-    ifstream fileGuardian("Guardianes.txt");
-    if (!fileGuardian.is_open()) {
-    	cout << endl;
-        cout << "Archivo Guardianes.txt no encontrado." << endl;
-        cout << "No se ha podido abrir correctamente." << endl;
-        return 1;
-    } else {
-    	cout << endl;
-        cout << "Archivo Guardianes.txt encontrado.";
-        cout << "...Archivo abierto satisfactoriamente" << endl;
-    }
 
+//---------------------------------------------COMIENZO DE LA FUNCION MAIN----------------------------------------------
+int main (){
+
+	Grafo grafo;
+    unordered_set<string> uniqueCity;
+    lecturaCiudad("Ciudades.txt", grafo, uniqueCity);
+    
+    // Función que carga los datos del archivo "Guardianes.txt".
+    vector<pair<int, Guardian>> guardianes = leerGuardianesDesdeArchivo("Guardianes.txt");
+    
+    // Asignar numeros a las ciudades.
+    unordered_map<string, int> numCiudad;
+    int numero = 1;
+    for (const auto &ciudad : uniqueCity) {
+        numCiudad[ciudad] = numero;
+        ++numero;
+    }
+    
+    // Imprimir asignación de numeros a ciudades.
+    cout << "\n[ASIGNACION NUMEROS A CIUDADES]: \n";
+    cout << endl;
+    for (const auto &par : numCiudad) {
+        cout << par.second << ":" << par.first << "\n";
+    }
+	
+	// Mapeado de Guardianes con numero asignado.
+    unordered_map<int, Guardian> numeroGuardianMap;
+    for (const auto& par : guardianes) {
+        numeroGuardianMap[par.first] = par.second;
+    }
+    
 	// Menu principal del juego
 	bool cerrar = false;
 	while (!cerrar){
@@ -135,16 +255,57 @@ int main (){
         	case 1:{
 				break;
 			}
-			case 2:{
-				
-				break;
+			case 2: {
+			    int numeroGuardian;
+			    
+			    cout << endl;
+			    // Mostrar la lista de guardianes con sus números asignados
+			    cout << "[LISTA DE GUARDIANES]" << endl;
+			    for (const auto& par : guardianes) {
+			        cout << par.first << "-" << par.second.name << endl;
+			    }
+			    
+			    cout << "Ingrese numero de Guardian a conocer: ";
+			    cin >> numeroGuardian;
+			
+			    auto it = numeroGuardianMap.find(numeroGuardian);
+			    if (it != numeroGuardianMap.end()) {
+			        cout << endl;
+			        cout << "Nombre: " << it->second.name << endl;
+			        cout << "Nivel de Poder: " << it->second.powerLevel << endl;
+			        cout << "Maestro: " << it->second.master << endl;
+			        cout << "Ciudad: " << it->second.city << endl;
+			    } else {
+			        cout << "!Oops¡ Numero ingresado no valido." << endl;
+			    }
+			
+			    break;
 			}
 			case 3:{
 				
-				break;
+				int grafo1;
+				cout << "---------------------BIENVENIDOS AL REINO---------------------" << endl;
+				imprimirGrafo(grafo);
+				
+				cout << endl;
+				cout << "Te gustaria ver nuestro reino en forma de Matriz? ";
+				cout << "[1=Si / 2=No]" << endl;
+				cin >> grafo1;
+				
+				if (grafo1 = 1){
+					// Impresion de Matriz de Adyacencia con numeros.
+					MatrizComparacion(uniqueCity, grafo, numCiudad);
+				} else if (grafo1 = 2){
+					cout << "Lo lamento don BUEN PROGRAMADOOR, pero de momento son las unicas formas de visitar el reino" << endl;
+				} else {
+					break;
+				}
 			}
 			case 4:{
-				
+				cout << endl;
+				cout << "Los Guardianes se encuentran en la Super Guardia Super Secreta haciendo cosas de Guardianes super Valerosos (Dormir arropados placidamente)" << endl;
+				cout << "Por lo que ahora los Guardianes no tienen fuerzas para luchar" << endl;
+				cout << "PERO SIGUE ASI FORASTERO, ME GUSTA TU INSISTENCIA" << endl;
 				break;
 			}
 			case 5:{
